21337d5122df91480c81a9fd7c78ee04d1a7273e
praveenkumarj27 <jpraveenkumar263@gmail.com>
2022-10-18 20:14:29 +0530

    heroku

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,5 +1,4 @@
-// export const host = "https://tex-chat.herokuapp.com";
-export const host = "http://localhost:8000";
+ export const host = "https://tex-chat.herokuapp.com";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
